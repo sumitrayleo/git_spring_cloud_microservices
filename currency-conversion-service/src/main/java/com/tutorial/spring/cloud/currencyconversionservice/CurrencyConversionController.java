4dfd2589ac4725000fc3a51d4153de45d625c896
Sumit Ray <sumit.ray@ehi.com>
2018-06-20 18:48:21 +0530

    Integrate with Currency Exchange

---
+++ currency-conversion-service/src/main/java/com/tutorial/spring/cloud/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,32 @@
+package com.tutorial.spring.cloud.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionResponse returnCurrencyCalculatedValue(@PathVariable String from, @PathVariable String to,
+        @PathVariable BigDecimal quantity) {
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversionResponse> responseEntity = new RestTemplate().getForEntity(
+            "http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionResponse.class, uriVariables);
+        CurrencyConversionResponse currencyConversionResponse = responseEntity.getBody();
+        return new CurrencyConversionResponse(currencyConversionResponse.getId(), from, to,
+            currencyConversionResponse.getConversionMultiple(), quantity,
+            quantity.multiply(currencyConversionResponse.getConversionMultiple()), currencyConversionResponse.getPort());
+    }
+
+}
---
+++ currency-conversion-service/src/main/java/com/tutorial/spring/cloud/currencyconversionservice/CurrencyConversionResponse.java
@@ -0,0 +1,87 @@
+package com.tutorial.spring.cloud.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversionResponse {
+    
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedValue;
+    private int port;
+    
+    public CurrencyConversionResponse() {
+        
+    }
+    
+    public CurrencyConversionResponse(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity,
+                    BigDecimal totalCalculatedValue, int port) {
+        super();
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+        this.quantity = quantity;
+        this.totalCalculatedValue = totalCalculatedValue;
+        this.port = port;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotalCalculatedValue() {
+        return totalCalculatedValue;
+    }
+
+    public void setTotalCalculatedValue(BigDecimal totalCalculatedValue) {
+        this.totalCalculatedValue = totalCalculatedValue;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+    
+}
