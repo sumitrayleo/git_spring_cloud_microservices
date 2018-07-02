aa000c8add54ab95817d937affdeed11a3c4251d
Sumit Ray <sumit.ray@ehi.com>
2018-07-02 19:10:32 +0530

    Distributing calls using Eureka and Ribbon

--- currency-conversion-service/src/main/java/com/tutorial/spring/cloud/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/tutorial/spring/cloud/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service") // path="localhost:8000")
+@FeignClient(name="currency-exchange-service") 
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
