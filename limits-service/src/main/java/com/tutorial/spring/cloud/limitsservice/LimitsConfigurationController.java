602bbb1d03e68504ce7d16466a7ebf170a8adbd4
Sumit Ray <sumit.ray@ehi.com>
2018-07-06 20:18:08 +0530

    Added Circuit Breaker or Fault tolerance with Hysterix

--- limits-service/src/main/java/com/tutorial/spring/cloud/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/tutorial/spring/cloud/limitsservice/LimitsConfigurationController.java
@@ -4,19 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.tutorial.spring.cloud.limitsservice.bean.LimitConfiguration;
 import com.tutorial.spring.cloud.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitsConfigurationController {
-    
+
     @Autowired
     private Configuration configuration;
     
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsConfiguration() {
-        
         return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
     }
 
+    @GetMapping("/limits/fault-tolerance")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveLimitsConfiguration")
+    public LimitConfiguration retrieveFaultTolerantLimitsConfiguration() {
+        throw new RuntimeException("Not Available");
+    }
+
+    public LimitConfiguration fallbackRetrieveLimitsConfiguration() {
+        return new LimitConfiguration(8, 888);
+    }
 }
